d69391214c69e72af1270098da1d5558ec4a8781
mezume <126116116+Mezumesocial@users.noreply.github.com>
2023-02-25 17:24:17 +0500

    aa

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Search from './Search';
 function App() {
   return (
     <>
-    <canvas role="img" aria-label="A tree growing until it bears fruit, dropping its fruit, shrinking, and repeating the cycle"></canvas>
     <Search />
 
     </>
